1dc9b0c8b688d451250375556403fbc8b0bb568a
Mindy <meetup@yomimono.org>
2019-10-04 16:23:43 -0400

    wip: c64stitch

---
+++ c64say/src/c64stitch.ml
@@ -0,0 +1,40 @@
+open Stitchy.Types
+(* take a phrase, output a stitch file *)
+
+let phrase = Cmdliner.Arg.(value & pos 0 string "HELLO\\nWORLD" & info [])
+let interline =
+  let doc = "extra space to insert between lines (in stitches)" in
+  Cmdliner.Arg.(value & opt int 0 & info ["interline"] ~doc)
+
+(* TODO: background and text color ought to be choosable from the known C64 colors. *)
+
+(* TODO: grid size should be choosable from the known valid values. *)
+
+let substrate_of_phrase phrase interline =
+  let {C64chars.height; width; _ } = C64chars.get_dimensions phrase interline in
+  { Stitchy.Types.background = (255, 255, 255);
+    grid = Fourteen;
+    max_x = max 0 (width - 1);
+    max_y = max 0 (height - 1);
+    block_size = 1;
+  }
+
+let blocks_of_phrase block phrase interline =
+  let add_blocks_for_letter ~x_off ~y_off letter blockmap =
+    match C64chars.CharMap.find_opt letter C64chars.map with
+    | None -> blockmap
+    | Some pixels -> List.fold_left (fun m (x, y) ->
+        BlockMap.add (x_off + x, y_off + y) block m
+      ) blockmap pixels
+  in
+  let add_line ~y_off line blockmap =
+    Astring.String.fold_left (fun (x_off, map) c ->
+        let map = add_blocks_for_letter ~x_off ~y_off c map in
+        (x_off + C64chars.w, map)
+      ) (0, blockmap) line
+  in
+  let blockmap = Stitchy.Types.BlockMap.empty in
+  List.fold_left (fun (y_off, map) line ->
+      let next_y_off = y_off + (C64chars.h + interline) in
+      (next_y_off, (snd @@ add_line ~y_off line map))
+    ) (0, blockmap) (C64chars.get_dimensions phrase interline).lines
