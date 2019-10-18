4fcb86655efe476fb8ae572bc8a484e7499ea833
Mindy <meetup@yomimono.org>
2019-10-18 16:57:12 -0400

    use a common fn for padding

--- embellish/lib/compose_stitch.ml
+++ embellish/lib/compose_stitch.ml
@@ -7,36 +7,29 @@ let displace_down ~amount ((x, y) : Block.t) =
 let displace_right ~amount ((x, y) : Block.t) =
   (x + amount), y
 
-let hpadding image1 image2 =
-  let bigger, smaller =
-    if image1.substrate.max_x > image2.substrate.max_x then image1, image2 else image2, image1
-  in
-  if bigger.substrate.max_x = smaller.substrate.max_x then 0, 0
-  else begin
-    let difference = bigger.substrate.max_x - smaller.substrate.max_x in
-    if difference mod 2 = 0 then (difference / 2), (difference / 2)
-    else (difference / 2), (difference / 2 + 1)
-  end
-(* TODO: the caller assumes that image1 needs the padding *)
-let vpadding left right =
+
+let padding one two =
   let which_to_pad, bigger, smaller =
-    if left.substrate.max_y > right.substrate.max_y
-    then `Right, left, right
-    else `Left, right, left
+    if one > two
+    then `Second, one, two
+    else `First, two, one
   in
-  if bigger.substrate.max_y = smaller.substrate.max_y then `None, 0, 0
+  if one = two then `None, 0, 0
   else begin
-    let difference = bigger.substrate.max_y - smaller.substrate.max_y in
+    let difference = bigger - smaller in
     if difference mod 2 = 0 then (which_to_pad, (difference / 2), (difference / 2))
     else which_to_pad, ((difference / 2) + 1), (difference / 2)
   end
+
+let vpadding left right = padding left.substrate.max_y right.substrate.max_y
+let hpadding top bottom = padding top.substrate.max_x bottom.substrate.max_x
 
 let hcat_with_substrate substrate upper lower =
   let new_max_y = upper.substrate.max_y + lower.substrate.max_y + 1
   and new_max_x = max upper.substrate.max_x lower.substrate.max_x
   in
   let substrate = { substrate with max_x = new_max_x; max_y = new_max_y } in
-  let left_padding, _ = hpadding upper lower in
+  let which_to_pad, left_padding, _ = hpadding upper lower in
   let stitches = Stitchy.Types.BlockMap.fold (fun (x, y) v m ->
       BlockMap.add (displace_down ~amount:(upper.substrate.max_y + 1) (x, y)) v m
     ) lower.stitches BlockMap.empty in
