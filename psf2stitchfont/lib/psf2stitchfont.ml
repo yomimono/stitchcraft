9f4fb7ddeb4ce5ed1a74f213801e57099fdc1d88
Mindy <meetup@yomimono.org>
2020-03-19 20:32:40 -0500

    add initial proof-of-concept for psf parser

---
+++ psf2stitchfont/lib/psf2stitchfont.ml
@@ -0,0 +1,96 @@
+[%%cstruct
+  type psf2header = {
+    magic : uint8_t [@len 4];
+    version : uint32_t;
+    start_of_glyphs : uint32_t;
+    flags : uint32_t;
+    number_of_glyphs : uint32_t;
+    bytes_per_character : uint32_t;
+    height : uint32_t; (* in bits *)
+    width : uint32_t; (* also in bits *)
+  } [@@little_endian]]
+
+let psf2magic = Cstruct.of_string "\x72\xb5\x4a\x86"
+
+let pp_error fmt = function
+  | `Too_short -> Format.fprintf fmt "file too small to contain valid psf2 header"
+  | `Wrong_magic c -> Format.fprintf fmt "file is not a valid psf2 file (wrong magic: %a)" Cstruct.hexdump_pp c
+  | `No_unicode -> Format.fprintf fmt "file is not unicode-ready according to psf2 header flags"
+  | `No_unicode_info -> Format.fprintf fmt "file contains no unicode information"
+
+(* if height and weight aren't evenly divisible by 8,
+   they'll be padded to fit (probably with 0s, since this is a bitmap font) *)
+let pad_to_8 n =
+  if n mod 8 = 0 then n
+  else n + (8 - (n mod 8))
+
+let high_bit_set byte = byte land 0x80 = 0x80
+
+let to_bit_list byte =
+  Printf.printf "getting bits set in %x\n%!" (int_of_char byte);
+  let rec aux l index byte = match (int_of_char byte) land 0xff with
+    | 0 -> l
+    | nonzero when high_bit_set nonzero ->
+      let byte = (int_of_char byte) lsl 1 |> (land) 0xff |> char_of_int in
+      aux (index :: l)  (index + 1) byte
+    | _unset_bit ->
+      let byte = (int_of_char byte) lsl 1 |> (land) 0xff |> char_of_int in
+      aux l (index + 1) byte
+  in
+  aux [] 0 byte
+
+let read_row ~y row ~width =
+  Printf.printf "reading row of width %d, at y-index %d\n%!" width y;
+  let rec aux bits row index =
+    if index >= width || Cstruct.len row = 0 then bits
+    else begin
+      let bit_list = to_bit_list (Cstruct.get_char row 0) in
+      let new_bits = List.map (fun bit_number -> (bit_number + 8*index, y)) bit_list in
+      aux (new_bits @ bits) (Cstruct.shift row 1) (index + 1)
+    end
+  in
+  let stitches = aux [] row 0 in
+  Printf.printf "got %d stitches\n%!" @@ List.length stitches;
+  stitches
+
+let rec read_rows buffer ~bytes_in_row ~height ~width rows index =
+  if index >= height then rows
+  else begin
+    let row = read_row ~y:index (Cstruct.sub buffer 0 bytes_in_row) ~width in
+    read_rows (Cstruct.shift buffer bytes_in_row) ~bytes_in_row ~height ~width (row @ rows) (index + 1)
+  end
+
+let parse_glyph_table ~width ~height table =
+  let bytes_in_row = width / 8 in
+  let rec next_glyph glyphs table =
+    if Cstruct.len table < bytes_in_row * height then glyphs
+    else begin
+      let stitches = read_rows table ~bytes_in_row ~height ~width [] 0 in
+      let glyph : Stitchy.Types.glyph = {
+        stitches;
+        height;
+        width;
+      } in
+      next_glyph (glyph :: glyphs) (Cstruct.shift table (bytes_in_row * height))
+    end
+  in
+  next_glyph [] table
+
+
+let glyphmap_of_psf_header buffer =
+  if Cstruct.len buffer < 0x20 then Error `Too_short
+  else if 0 <> Cstruct.compare psf2magic (get_psf2header_magic buffer) then Error (`Wrong_magic (get_psf2header_magic buffer))
+  else if get_psf2header_flags buffer <> 0x01l then Error `No_unicode (* TODO: we can probably do something else here *)
+  else begin
+    let glyphs_size = Int32.mul (get_psf2header_number_of_glyphs buffer) (get_psf2header_bytes_per_character buffer) in
+    let unicode_start = Int32.add (get_psf2header_start_of_glyphs buffer) glyphs_size in
+    let unicode_length = (Cstruct.len buffer) - (Int32.to_int unicode_start) in
+    let glyph_table = Cstruct.sub buffer (Int32.to_int (get_psf2header_start_of_glyphs buffer)) (Int32.to_int glyphs_size) in
+    if unicode_length < 0 then Error `No_unicode_info
+    else begin
+      let glyphs = parse_glyph_table
+          ~width:(get_psf2header_width buffer |> Int32.to_int)
+          ~height:(get_psf2header_height buffer |> Int32.to_int) glyph_table in
+      Ok (`Glyphmap (glyphs, unicode_length))
+    end
+  end
---
+++ psf2stitchfont/src/readpsf.ml
@@ -0,0 +1,55 @@
+let input =
+  let doc = "file from which to read. -, the default, is stdin." in
+  Cmdliner.Arg.(value & pos 0 string "-" & info [] ~doc)  
+
+let info =
+  let doc = "ingest psf (pc screen font) version 2 files" in
+  Cmdliner.Term.info "readpsf" ~doc
+
+let white = match Stitchy.DMC.Thread.of_rgb (255, 255, 255) with
+  | None -> assert false
+  | Some white -> white
+
+let print_glyph fmt (glyph : Stitchy.Types.glyph) =
+  let block : Stitchy.Types.block = {
+    thread = white;
+    stitch = Stitchy.Types.Full;
+  } in
+  let blockmap = List.fold_left
+      (fun m coordinate -> Stitchy.Types.BlockMap.add coordinate block m)
+      Stitchy.Types.BlockMap.empty
+      glyph.stitches
+  in
+  let substrate : Stitchy.Types.substrate = {
+    grid = Stitchy.Types.Fourteen;
+    background = (0, 0, 0);
+    max_x = glyph.width - 1;
+    max_y = glyph.height - 1;
+  } in
+  let state : Stitchy.Types.state = { stitches = blockmap; substrate; } in
+  Format.fprintf fmt "%a\n%!" Stitchy.Types.pp_state state;
+  Format.fprintf fmt "stitches: %a" Fmt.(list @@ pair int int) glyph.stitches
+
+let read input =
+  match Bos.OS.File.read (Fpath.v input) with
+  | Error e -> Error e
+  | Ok s ->
+    let buffer = Cstruct.of_string s in
+    match Psf2stitchfont.glyphmap_of_psf_header buffer with
+    | Error e ->
+      Format.eprintf "%a\n%!" Psf2stitchfont.pp_error e;
+      Error (`Msg "parsing failed")
+    | Ok (`Glyphmap (glyphs, _unicode)) ->
+      Format.printf "%a\n%!" (Fmt.list print_glyph) glyphs;
+      Ok ()
+
+let read_t = Cmdliner.Term.(const read $ input)
+
+let () =
+  Cmdliner.Term.eval (read_t, info) |> function
+  | `Ok (Error (`Msg s)) -> 
+    Format.eprintf "%s\n%!" s; exit 1
+  | `Ok (Ok ()) ->
+    exit 0 
+  | `Error _ -> exit 1
+  | e -> Cmdliner.Term.exit e
