b652566d76a804b75d53802daae37d0ae11ef05e
yomimono <maintenance@identity-function.com>
2021-06-19 15:52:35 -0500

    I don't like it, but I didn't break it any more, at least

--- patreader/src/backstitch.ml
+++ patreader/src/backstitch.ml
@@ -2,7 +2,7 @@ let eq_layers_disregarding_stitch_set layer stitch thread =
   Stitchy.Types.(equal_stitch layer.stitch stitch) &&
   Stitchy.DMC.Thread.compare layer.thread thread = 0
 
-let add_stitches layers thread stitch new_stitches =
+let add_stitches layers thread stitch (new_stitches : Stitchy.Types.CoordinateSet.elt list) =
   match List.partition (fun layer ->
       eq_layers_disregarding_stitch_set layer stitch thread) layers with
   | layer::_, other_layers ->
@@ -122,29 +122,39 @@ let specific_stitches backstitch =
   let open Patreader in
   if backstitch.start_x = backstitch.end_x && backstitch.start_y = backstitch.end_y then begin
     match backstitch.start_position, backstitch.end_position with
-    | (1, 3) | (3, 1) -> Ok (Stitchy.Types.Back Top, [(backstitch.start_x - 1, backstitch.start_y - 1)])
-    | (1, 7) | (7, 1) -> Ok (Stitchy.Types.Back Left, [(backstitch.start_x - 1, backstitch.start_y - 1)])
-    | (3, 9) | (9, 3) -> Ok (Stitchy.Types.Back Right, [(backstitch.start_x - 1, backstitch.start_y - 1)])
-    | (7, 9) | (9, 7) -> Ok (Stitchy.Types.Back Bottom, [(backstitch.start_x - 1, backstitch.start_y - 1)])
+    | (1, 3) | (3, 1) -> Ok (Stitchy.Types.Back Top, (backstitch.start_x - 1, backstitch.start_y - 1))
+    | (1, 7) | (7, 1) -> Ok (Stitchy.Types.Back Left, (backstitch.start_x - 1, backstitch.start_y - 1))
+    | (3, 9) | (9, 3) -> Ok (Stitchy.Types.Back Right, (backstitch.start_x - 1, backstitch.start_y - 1))
+    | (7, 9) | (9, 7) -> Ok (Stitchy.Types.Back Bottom, (backstitch.start_x - 1, backstitch.start_y - 1))
     | _ -> Error (`Msg (Format.asprintf "unrepresentable stitch %a" pp_backstitch backstitch))
   end else Error (`Msg (Format.asprintf "unrepresentable stitch %a" pp_backstitch backstitch))
 
-let rec break_backstitch acc backstitch =
-  let backstitch = normalize_backstitch backstitch in
-  (* degenerate case; a "backstitch" with only one point is nothing *)
-  if backstitch.start_x = backstitch.end_x &&
-     backstitch.start_y = backstitch.end_y && 
-     backstitch.start_position = backstitch.end_position
-  then acc
-  (* base case; we can't break this down any further *)
-  else if backstitch.start_x = backstitch.end_x &&
-     backstitch.start_y = backstitch.end_y && 
-     backstitch.start_position <> backstitch.end_position
-  then specific_stitches backstitch :: acc
-  (* a horizontal line of >1 cell *)
-  else if backstitch.start_y = backstitch.end_y &&
-          backstitch.start_x <> backstitch.end_x
-  then begin
+type backstitch_bundle = Stitchy.Types.stitch * (int * int) list
+
+let rec break_backstitch acc backstitch : ((backstitch_bundle, [`Msg of string]) result) =
+  let open Patreader in
+  match acc with
+  | Error _ as e -> e
+  | Ok (_s, prev_coords) as acc ->
+    let backstitch = normalize_backstitch backstitch in
+    (* degenerate case; a "backstitch" with only one point is nothing *)
+    if backstitch.start_x = backstitch.end_x &&
+       backstitch.start_y = backstitch.end_y && 
+       backstitch.start_position = backstitch.end_position
+    then acc
+    (* base case; we can't break this down any further *)
+    else if backstitch.start_x = backstitch.end_x &&
+            backstitch.start_y = backstitch.end_y && 
+            backstitch.start_position <> backstitch.end_position
+    then begin
+      match specific_stitches backstitch with
+      | Error _ as e -> e
+      | Ok (stitch_type, stitch_coordinates) -> Ok (stitch_type, (stitch_coordinates :: prev_coords))
+    end
+    (* a horizontal line of >1 cell *)
+    else if backstitch.start_y = backstitch.end_y &&
+            backstitch.start_x <> backstitch.end_x
+    then begin
 
 (*
        0         1         2
@@ -154,25 +164,30 @@ let rec break_backstitch acc backstitch =
 
    7   8   9/7   8   9/7   8   9
 *)
-    (* since the stitch has been normalized,
-     * we can assume that the above is expressed as (0, 0):1 to (0, 1):3 *)
-    let this_stitch = specific_stitches {backstitch with end_x = start_x + 1} in
-    break_backstitch (this_stitch :: acc) {backstitch with start_x = start_x + 1}
-  end
-  (* a vertical line of >1 cell *)
-  else if backstitch.start_x = backstitch.end_x &&
-          backstitch.start_y <> backstitch.end_y
-  then begin
-    let this_stitch = specific_stitches {backstitch with end_y = start_y + 1} in
-    break_backstitch (this_stitch :: acc) {backstitch with start_y = start_y + 1}
-  end
-  else begin
-    let msg = Format.asprintf "unrepresentable, unbreakable backstitch: %a" pp_backstitch backstitch in
-    Error `Msg msg::acc
-  end
+      (* since the stitch has been normalized,
+       * we can assume that the above is expressed as (0, 0):1 to (0, 1):3 *)
+      match specific_stitches {backstitch with end_x = backstitch.start_x + 1} with
+      | Error _ as e -> e
+      | Ok (stitch_type, stitch_coordinates) ->
+        break_backstitch (Ok (stitch_type, (stitch_coordinates::prev_coords)))
+          {backstitch with start_x = backstitch.start_x + 1}
+    end
+    (* a vertical line of >1 cell *)
+    else if backstitch.start_x = backstitch.end_x &&
+            backstitch.start_y <> backstitch.end_y
+    then begin
+      match specific_stitches {backstitch with end_y = backstitch.start_y + 1} with
+      | Error _ as e -> e
+      | Ok (stitch_type, stitch_coordinates) ->
+        break_backstitch (Ok (stitch_type, (stitch_coordinates :: prev_coords)))
+          {backstitch with start_y = backstitch.start_y + 1}
+    end
+    else begin
+      let msg = Format.asprintf "unrepresentable, unbreakable backstitch: %a" pp_backstitch backstitch in
+      Error (`Msg msg)
+    end
 
 let layers_of_backstitches threads backstitches =
-  let open Rresult.R in
   let one_backstitch acc backstitch =
     match acc with
     | Error e -> Error e
@@ -180,14 +195,9 @@ let layers_of_backstitches threads backstitches =
       match (List.nth threads (backstitch.Patreader.color_index - 1)) with
       | None -> Error (`Msg "unknown thread")
       | Some thread ->
-        match List.fold_left break_backstitch [] backstitch with
-        | Ok (stitch, stitches) ->
-          Format.eprintf "interpreting backstitch %a as stitch %a for coords\n%a\n%!"
-            Patreader.pp_backstitch backstitch
-            Stitchy.Types.pp_stitch stitch
-            Fmt.(list @@ parens @@ pair ~sep:Fmt.comma int int) stitches;
-          add_stitches layers thread stitch stitches >>= fun layers ->
-          Ok layers
+        match break_backstitch (Ok (Stitchy.Types.Back Top, [])) backstitch with
+        | Ok (stitch, coords) ->
+          add_stitches layers thread stitch coords
         | Error (`Msg s) ->
           Format.eprintf "%s\n%!" s;
           acc
