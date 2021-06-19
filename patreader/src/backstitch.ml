6fe6b6db9f91ff902e20cfb5bc84c216434df715
yomimono <maintenance@identity-function.com>
2021-06-19 10:51:30 -0500

    another shot at backstitch

---
+++ patreader/src/backstitch.ml
@@ -0,0 +1,165 @@
+let eq_layers_disregarding_stitch_set layer stitch thread =
+  Stitchy.Types.(equal_stitch layer.stitch stitch) &&
+  Stitchy.DMC.Thread.compare layer.thread thread = 0
+
+let add_stitches layers thread stitch new_stitches =
+  match List.partition (fun layer ->
+      eq_layers_disregarding_stitch_set layer stitch thread) layers with
+  | layer::_, other_layers ->
+    let stitches =
+      Stitchy.Types.CoordinateSet.(union layer.stitches @@ of_list new_stitches) in
+    Ok ({ layer with stitches } :: other_layers)
+  | [], other_layers ->
+    let layer = Stitchy.Types.{
+        thread; stitch; stitches = CoordinateSet.of_list new_stitches
+      } in
+    Ok (layer :: other_layers)
+
+(* the position numbers *appear* to be:
+ 1  2  3
+ 4  5  6
+ 7  8  9
+
+ where 1, 3, 7, and 9 are the vertices of the grid lines
+   and 5 is the center of the warp/weft overlap. *)
+
+(* this means position number 3 on coordinate (1, 0)
+ * is equivalent to position number 1 on coordinate (1, 1). *)
+(* normalize_backstitch attempts to take any stitches which are just
+ * one backstitch, but don't look like it because
+ * they're expressed as the same position on different stitches,
+ * and make them look like one backstitch.
+ *to show this example visually: *)
+
+(*
+       0         1         2
+   1   2   3/1---2---3/1   2   3
+
+0  4   5   6/4   5   6/4   5   6
+
+   7   8   9/7   8   9/7   8   9
+
+*)
+
+(* the stitch above is expressible as:
+ * (0, 0):3 to (1, 0):3
+ * (0, 0):3 to (2, 0):1
+ * (1, 0):1 to (1, 0):3
+ * (1, 0):1 to (2, 0):1
+ 
+   We prefer the third because it's the only representation
+   that maps nicely to the stitch grid we use for cross stitches,
+   and this whole thing is supposed to be about those. Remember? *)
+
+let normalize_backstitch backstitch =
+  let open Patreader in
+  match backstitch.start_position, backstitch.end_position with
+  (* top-side backstitches *)
+  | (3, 3) when (backstitch.end_x - backstitch.start_x) = 1 ->
+    {backstitch with start_x = backstitch.end_x;
+                     start_position = 1;
+    }
+  | (3, 1) when (backstitch.end_x - backstitch.start_x) = 2 ->
+    {backstitch with start_x = backstitch.start_x + 1;
+                     end_x = backstitch.end_x - 1;
+                     start_position = 1;
+                     end_position = 3; }
+  | (1, 1) when (backstitch.end_x - backstitch.start_x) = 1 ->
+    {backstitch with end_x = backstitch.start_x;
+                     end_position = 3;
+    }
+(*
+       0         1         2
+   1   2   3/1   2   3/1   2   3
+
+0  4   5   6/4   5   6/4   5   6
+
+   7   8   9 7   8   9 7   8   9
+   /   /    /    /    /    /   /
+   1   2   3 1   2   3 1   2   3
+
+1  4   5   6/4   5   6/4   5   6
+
+   7   8   9/7   8   9/7   8   9
+*)
+  (* right-side backstitch *)
+  | (9, 9) when (backstitch.end_y - backstitch.start_y) = 1 ->
+    { backstitch with start_y = backstitch.start_y + 1;
+                      start_position = 3;
+    }
+  | (9, 3) when (backstitch.end_y - backstitch.start_y) = 2 ->
+    { backstitch with start_y = backstitch.start_y + 1;
+                      end_y = backstitch.start_y - 1;
+                      start_position = 3;
+                      end_position = 9;
+    }
+  | (3, 3) when (backstitch.end_y - backstitch.start_y) = 1 ->
+    { backstitch with end_y = backstitch.start_y;
+                     end_position = 9;
+    }
+  (* left-side backstitch *)
+  | (7, 7) when (backstitch.end_y - backstitch.start_y) = 1 ->
+    { backstitch with start_y = backstitch.start_y + 1;
+                      start_position = 1;
+    }
+  | (7, 1) when (backstitch.end_y - backstitch.start_y) = 2 ->
+    { backstitch with start_y = backstitch.start_y + 1;
+                      end_y = backstitch.start_y - 1;
+                      start_position = 1;
+                      end_position = 7;
+    }
+  | (1, 1) when (backstitch.end_y - backstitch.start_y) = 1 ->
+    { backstitch with end_y = backstitch.start_y;
+                     end_position = 7;
+    }
+  (* bottom-side backstitch *)
+  | (9, 9) when (backstitch.end_x - backstitch.start_x) = 1 ->
+    { backstitch with start_x = backstitch.start_x + 1;
+                      start_position = 7;
+    }
+  | (7, 9) when (backstitch.end_x - backstitch.start_x) = 2 ->
+    { backstitch with start_x = backstitch.start_x + 1;
+                      end_x = backstitch.start_x - 1;
+                      start_position = 7;
+                      end_position = 9;
+    }
+  | (7, 7) when (backstitch.end_x - backstitch.start_x) = 1 ->
+    { backstitch with end_x = backstitch.start_x;
+                      end_position = 9;
+    }
+  | _ -> backstitch
+
+let specific_stitches backstitch =
+  let open Patreader in
+  let backstitch = normalize_backstitch backstitch in
+  if backstitch.start_x = backstitch.end_x && backstitch.start_y = backstitch.end_y then begin
+    match backstitch.start_position, backstitch.end_position with
+    | (1, 3) | (3, 1) -> Ok (Stitchy.Types.Back Top, [(backstitch.start_x - 1, backstitch.start_y - 1)])
+    | (1, 7) | (7, 1) -> Ok (Stitchy.Types.Back Left, [(backstitch.start_x - 1, backstitch.start_y - 1)])
+    | (3, 9) | (9, 3) -> Ok (Stitchy.Types.Back Right, [(backstitch.start_x - 1, backstitch.start_y - 1)])
+    | (7, 9) | (9, 7) -> Ok (Stitchy.Types.Back Bottom, [(backstitch.start_x - 1, backstitch.start_y - 1)])
+    | _ -> Error (`Msg (Format.asprintf "unrepresentable stitch %a" pp_backstitch backstitch))
+  end else Error (`Msg (Format.asprintf "unrepresentable stitch %a" pp_backstitch backstitch))
+
+let layers_of_backstitches threads backstitches =
+  let open Rresult.R in
+  let one_backstitch acc backstitch =
+    match acc with
+    | Error e -> Error e
+    | Ok layers ->
+      match (List.nth threads (backstitch.Patreader.color_index - 1)) with
+      | None -> Error (`Msg "unknown thread")
+      | Some thread ->
+        match specific_stitches backstitch with
+        | Ok (stitch, stitches) ->
+          Format.eprintf "interpreting backstitch %a as stitch %a for coords\n%a\n%!"
+            Patreader.pp_backstitch backstitch
+            Stitchy.Types.pp_stitch stitch
+            Fmt.(list @@ parens @@ pair ~sep:Fmt.comma int int) stitches;
+          add_stitches layers thread stitch stitches >>= fun layers ->
+          Ok layers
+        | Error (`Msg s) ->
+          Format.eprintf "%s\n%!" s;
+          acc
+  in
+  List.fold_left one_backstitch (Ok []) backstitches
--- patreader/src/translator.ml
+++ patreader/src/translator.ml
@@ -19,23 +19,6 @@ let to_substrate fabric =
   let max_x, max_y = fabric.Patreader.width - 1, fabric.Patreader.height - 1 in
   Stitchy.Types.{ grid; background = (r, g, b); max_x; max_y }
 
-let eq_layers_disregarding_stitch_set layer stitch thread =
-  Stitchy.Types.(equal_stitch layer.stitch stitch) &&
-  Stitchy.DMC.Thread.compare layer.thread thread = 0
-
-let add_stitches layers thread stitch new_stitches =
-  match List.partition (fun layer ->
-      eq_layers_disregarding_stitch_set layer stitch thread) layers with
-  | layer::_, other_layers ->
-    let stitches =
-      Stitchy.Types.CoordinateSet.(union layer.stitches @@ of_list new_stitches) in
-    Ok ({ layer with stitches } :: other_layers)
-  | [], other_layers ->
-    let layer = Stitchy.Types.{
-        thread; stitch; stitches = CoordinateSet.of_list new_stitches
-      } in
-    Ok (layer :: other_layers)
-
 let layers_of_cross_stitches threads stitches =
   List.fold_left (fun acc ((x, y), color_index, stitch) ->
       match acc, color_index with 
@@ -46,73 +29,8 @@ let layers_of_cross_stitches threads stitches =
         match (List.nth threads (color_index - 1)) with
         | None -> Error (`Msg "unknown thread")
         | Some thread ->
-          add_stitches acc thread stitch [(x, y)]
+          Backstitch.add_stitches acc thread stitch [(x, y)]
     ) (Ok []) stitches
-
-(* the position numbers *appear* to be:
- 1  2  3
- 4  5  6
- 7  8  9 *)
-
-let backstitch_of_position_pair backstitch =
-  let open Patreader in
-  match backstitch.start_position, backstitch.end_position with
-  | (1, 3) | (3, 1) when backstitch.start_y = backstitch.end_y -> Ok Stitchy.Types.Top
-  | (1, 1) | (3, 3) when backstitch.start_y = backstitch.end_y -> Ok Stitchy.Types.Top
-  | (7, 9) | (9, 7) when backstitch.start_y = backstitch.end_y -> Ok Stitchy.Types.Bottom
-  | (7, 7) | (9, 9) when backstitch.start_y = backstitch.end_y -> Ok Stitchy.Types.Bottom
-  | (1, 7) | (7, 1) when backstitch.start_x = backstitch.end_x -> Ok Stitchy.Types.Left
-  | (1, 1) | (3, 3) when backstitch.start_x = backstitch.end_x -> Ok Stitchy.Types.Left
-  | (3, 9) | (9, 3) when backstitch.start_x = backstitch.end_x -> Ok Stitchy.Types.Right
-  | (7, 7) | (9, 9) when backstitch.start_x = backstitch.end_x -> Ok Stitchy.Types.Right
-  | _ -> Error (`Msg (Format.asprintf "unrepresentable backstitch %a" pp_backstitch backstitch))
-
-let stitches_of_backstitch backstitch =
-  let open Rresult.R in
-  let open Patreader in
-  let extra_stitch = match backstitch.start_position, backstitch.end_position with
-    | (1, 3) | (3, 1) -> 1
-    | (1, 7) | (7, 1) -> 1
-    | (3, 9) | (9, 3) -> 1
-    | (7, 9) | (9, 7) -> 1
-    | _ -> 0
-  in
-  let diff one two = (abs (two - one)) + extra_stitch in
-  let horizontal backstitch =
-    let range = diff backstitch.end_x backstitch.start_x in
-    List.init range (fun more_x -> (backstitch.start_x - 1 + more_x, backstitch.start_y - 1))
-  and vertical backstitch =
-    let range = diff backstitch.end_y backstitch.start_y in
-    List.init range (fun more_y -> (backstitch.start_x - 1, backstitch.start_y - 1 + more_y)) 
-  in
-  backstitch_of_position_pair backstitch >>| function
-  | Top -> Stitchy.Types.Back Top, horizontal backstitch
-  | Bottom -> Stitchy.Types.Back Bottom, (horizontal backstitch)
-  | Left -> Stitchy.Types.Back Left, (vertical backstitch)
-  | Right -> Stitchy.Types.Back Right, (vertical backstitch)
-
-let layers_of_backstitches threads backstitches =
-  let open Rresult.R in
-  let one_backstitch acc backstitch =
-    match acc with
-    | Error e -> Error e
-    | Ok layers ->
-      match (List.nth threads (backstitch.Patreader.color_index - 1)) with
-      | None -> Error (`Msg "unknown thread")
-      | Some thread ->
-        match stitches_of_backstitch backstitch with
-        | Ok (stitch, stitches) ->
-          Format.eprintf "interpreting backstitch %a as stitch %a for coords\n%a\n%!"
-            Patreader.pp_backstitch backstitch
-            Stitchy.Types.pp_stitch stitch
-            Fmt.(list @@ parens @@ pair ~sep:Fmt.comma int int) stitches;
-          add_stitches layers thread stitch stitches >>= fun layers ->
-          Ok layers
-        | Error (`Msg s) ->
-          Format.eprintf "%s\n%!" s;
-          acc
-  in
-  List.fold_left one_backstitch (Ok []) backstitches
 
 let to_stitches (_fabric, _metadata, palette, stitches, backstitches) =
   let open Rresult.R in
@@ -126,6 +44,6 @@ let to_stitches (_fabric, _metadata, palette, stitches, backstitches) =
    * referring to it later. *)
   let threads = List.map match_thread palette in
   layers_of_cross_stitches threads stitches >>= fun stitch_layers ->
-  layers_of_backstitches threads backstitches >>= fun backstitch_layers ->
+  Backstitch.layers_of_backstitches threads backstitches >>= fun backstitch_layers ->
   Ok (stitch_layers @ backstitch_layers)
 
