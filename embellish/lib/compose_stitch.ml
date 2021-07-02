5e3273f32b3e5896228e107969f5b6291206f617
yomimono <maintenance@identity-function.com>
2021-07-02 18:20:15 -0500

    add corner rotation option to embellish_stitch

--- embellish/lib/compose_stitch.ml
+++ embellish/lib/compose_stitch.ml
@@ -15,6 +15,8 @@ let shift_stitches_right ~amount (layer : layer) =
   {layer with stitches =
                 CoordinateSet.map (displace_stitch_right ~amount) layer.stitches}
 
+let shift_layers_down ~amount = List.map (shift_stitches_down ~amount)
+let shift_layers_right ~amount = List.map (shift_stitches_right ~amount)
 
 let padding one two =
   let which_to_pad, bigger, smaller =
@@ -25,7 +27,7 @@ let padding one two =
   if one = two then `None, 0, 0
   else begin
     let difference = bigger - smaller in
-    (* for an even difference,pad evenly on both sides *)
+    (* for an even difference, pad evenly on both sides *)
     if difference mod 2 = 0 then (which_to_pad, (difference / 2), (difference / 2))
     (* for an odd difference, favor padding the top or left element *)
     else which_to_pad, ((difference / 2) + 1), (difference / 2)
@@ -109,15 +111,94 @@ let rec hrepeat image = function
   | n when n <= 1 -> image
   | n -> hcat image (hrepeat image (n - 1))
 
+let rotate_ccw (pattern : pattern) =
+  let rotate (x, y) =
+    let rotated_x = x * -1 in
+    let transposed_x = rotated_x + pattern.substrate.max_x in
+    (y, transposed_x)
+  in
+  (* TODO yet again, backstitch *)
+  let (layers : layer list) = List.map (fun (layer : layer) ->
+      { layer with stitches = CoordinateSet.map rotate layer.stitches }
+    ) pattern.layers in
+  let substrate = {pattern.substrate with max_x = pattern.substrate.max_y;
+                                          max_y = pattern.substrate.max_x} in
+  { pattern with layers = layers; substrate = substrate }
+
 let (<->) = hcat
 let (<|>) = vcat
 
 let empty base_substrate max_x max_y =
   let substrate = {base_substrate with max_x; max_y } in
-  let layers = [] in
-  {layers; substrate; backstitch_layers = []}
+  {layers = []; substrate; backstitch_layers = []}
 
-let embellish ~center ~corner ~top ~side =
+(* TODO: this definitely needs a better name *)
+let better_embellish ~corner ~top ~center =
+  let horizontal_repetitions =
+    let open Stitchy.Types in
+    let x_to_fill =
+      center.substrate.max_x + 2 * corner.substrate.max_y
+      - corner.substrate.max_x
+    in
+    x_to_fill / top.substrate.max_x +
+    (if x_to_fill mod top.substrate.max_x <> 0 then 1 else 0)
+  and vertical_repetitions =
+    let open Stitchy.Types in
+    let y_to_fill =
+      center.substrate.max_y + 2 * corner.substrate.max_y
+      - corner.substrate.max_x
+    in
+    (* we still use top.substrate.max_x here, because we'll
+     * be rotating the top pattern 90 degrees to use it on the sides *)
+    y_to_fill / top.substrate.max_x +
+    (if y_to_fill mod top.substrate.max_y <> 0 then 1 else 0)
+  in
+  let top_border = vrepeat top horizontal_repetitions
+  and left_border = hrepeat (rotate_ccw top) vertical_repetitions
+  in
+  let bottom_border = rotate_ccw @@ rotate_ccw top_border
+  and right_border = rotate_ccw @@ rotate_ccw left_border
+  in
+  (* the straight, repeated borders *)
+  let borders = [
+    shift_layers_right ~amount:(corner.substrate.max_x + 1) top_border.layers;
+    shift_layers_down ~amount:(corner.substrate.max_x + 1) @@
+      shift_layers_right ~amount:(corner.substrate.max_x + 1 + top_border.substrate.max_x + 1) right_border.layers;
+    shift_layers_right ~amount:(corner.substrate.max_y + 1) @@
+      shift_layers_down ~amount:(corner.substrate.max_x + 1 + left_border.substrate.max_y + 1) bottom_border.layers;
+    shift_layers_down ~amount:(corner.substrate.max_y + 1) left_border.layers;
+  ] |> List.flatten in
+  (* the corners *)
+  let corners = [
+    corner.layers; (* upper left *)
+    shift_layers_right ~amount:(corner.substrate.max_x + 1 + top_border.substrate.max_x + 1) (rotate_ccw @@ rotate_ccw @@ rotate_ccw corner).layers; (* upper right *)
+    shift_layers_right ~amount:(top_border.substrate.max_x + 1 + corner.substrate.max_y + 1) @@
+    shift_layers_down ~amount:(left_border.substrate.max_y + 1 + corner.substrate.max_x + 1) (rotate_ccw @@ rotate_ccw corner).layers; (* lower right *)
+    shift_layers_down ~amount:(corner.substrate.max_y + 1 + left_border.substrate.max_y + 1) (rotate_ccw corner).layers; (* lower left *)
+  ] |> List.flatten in
+  let center_shifted =
+    let left_padding = ((top_border.substrate.max_x + corner.substrate.max_y)
+                        - center.substrate.max_x) / 2 in
+    let top_padding = ((left_border.substrate.max_y + corner.substrate.max_y)
+                       - center.substrate.max_y) / 2 in
+    shift_layers_down ~amount:(corner.substrate.max_y + 1 + top_padding + 1) @@ 
+    shift_layers_right ~amount:(left_border.substrate.max_x + 1 + left_padding + 1) center.layers
+  in
+  let layers =
+    Stitchy.Layers.(merge_threads (center_shifted @
+                                   corners @ borders) [])
+  in
+  let new_substrate = { center.Stitchy.Types.substrate with
+                        max_x = corner.substrate.max_x + 1 +
+                                top_border.substrate.max_x + 1 +
+                                corner.substrate.max_y + 1;
+                        max_y = left_border.substrate.max_y + 1 +
+                                corner.substrate.max_x + 1 +
+                                corner.substrate.max_y + 1;
+                      } in
+  { substrate = new_substrate; layers; backstitch_layers = [] }
+
+let embellish ~rotate_corners ~center ~corner ~top ~side =
   let open Compose in
   let horiz_border_reps = border_repetitions
       ~center:(center.substrate.max_x + 1)
@@ -133,6 +214,13 @@ let embellish ~center ~corner ~top ~side =
   in
   let side_border = hrepeat side vert_border_reps in
   let top_border = vrepeat top horiz_border_reps in
+  (* upper-left, upper-right, lower-right, lower-left *)
+  let ul, ur, lr, ll =
+    match rotate_corners with
+    | false -> corner, corner, corner, corner
+    | true -> corner, (rotate_ccw @@ rotate_ccw @@ rotate_ccw corner),
+              rotate_ccw @@ rotate_ccw corner, rotate_ccw corner
+  in
   let center =
     if center.substrate.max_x < top_border.substrate.max_x then begin
       let x_difference = top_border.substrate.max_x - center.substrate.max_x in
@@ -143,8 +231,8 @@ let embellish ~center ~corner ~top ~side =
     end else
       (side_border <|> center <|> side_border)
   in
-  (corner <|> top_border <|> corner)
+  (ul <|> top_border <|> ur)
   <->
   center
   <->
-  (corner <|> top_border <|> corner)
+  (ll <|> top_border <|> lr)
