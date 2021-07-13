8151d13c7c2cde51f66ceae7742e1a73e9df2587
yomimono <maintenance@identity-function.com>
2021-07-13 15:34:57 -0500

    mostly-working backstitch at long last.

--- patreader/src/pat2stitchy.ml
+++ patreader/src/pat2stitchy.ml
@@ -3,23 +3,33 @@ let input =
   and docv = "FILE" in
   Cmdliner.Arg.(value & pos_all string [] & info [] ~doc ~docv)
 
+let verbose =
+  let doc = "print file metadata on stderr"
+  and docv = "VERBOSE" in
+  Cmdliner.Arg.(value & flag & info ["v";"verbose"] ~doc ~docv)
+
 let info =
   let doc = "read .pat files" in
   Cmdliner.Term.info "patreader" ~doc
 
-let read_one input =
+let spoo (fabric, metadata, palette, stitches, extras, knots, backstitches) =
+  Format.eprintf "metadata: %a\n%!" Patreader.pp_metadata metadata;
+  Format.eprintf "fabric: %a\n%!" Patreader.pp_fabric fabric;
+  Format.eprintf "palette: %a\n%!" Patreader.pp_palette palette;
+  Format.eprintf "got %d stitches\n%!" @@ List.length stitches;
+  Format.eprintf "got %d extras\n%!" @@ List.length extras;
+  Format.eprintf "got %d knots\n%!" @@ List.length knots;
+  Format.eprintf "got %d backstitches\n%!" @@ List.length backstitches;
+  ()
+
+let read_one verbose input =
   let open Lwt.Infix in
   Lwt_io.open_file ~mode:Input input >>= fun input ->
   Angstrom_lwt_unix.parse Patreader.file input >>= fun (_, result) ->
   match result with
   | Error e -> Lwt.return @@ Error (`Msg e)
   | Ok (fabric, metadata, palette, stitches, extras, knots, backstitches) ->
-    Format.eprintf "metadata: %a\n%!" Patreader.pp_metadata metadata;
-    Format.eprintf "fabric: %a\n%!" Patreader.pp_fabric fabric;
-    Format.eprintf "palette: %a\n%!" Patreader.pp_palette palette;
-    Format.eprintf "got %d extras\n%!" @@ List.length extras;
-    Format.eprintf "got %d knots\n%!" @@ List.length knots;
-    Format.eprintf "got %d backstitches\n%!" @@ List.length backstitches;
+    if verbose then spoo (fabric, metadata, palette, stitches, extras, knots, backstitches);
     let substrate = Translator.to_substrate fabric in
     (* TODO: this is probably not strictly correct; I think some entries in the
      * stitch list can be half, 3/4, etc stitches *)
@@ -27,24 +37,24 @@ let read_one input =
         coords, color, (Stitchy.Types.Cross Full)) stitches in
     match Translator.to_stitches (fabric, metadata, palette, stitches, backstitches) with
     | Error e -> Lwt.return @@ Error e
-    | Ok layers -> begin
+    | Ok (layers, backstitch_layers) -> begin
       let pattern = {Stitchy.Types.substrate = substrate;
-                     backstitch_layers = [];
+                     backstitch_layers;
                      layers } in
       Format.printf "%s" (Stitchy.Types.pattern_to_yojson pattern |> Yojson.Safe.to_string);
       Lwt.return (Ok ())
     end
 
-let main inputs =
+let main verbose inputs =
   List.fold_left (fun acc input ->
-      let res = Lwt_main.run @@ read_one input in
+      let res = Lwt_main.run @@ read_one verbose input in
       match acc, res with
       | e, Ok () -> e
       | Ok (), e -> e
       | Error (`Msg e1), Error (`Msg e2) -> Error (`Msg (e1 ^ "\n" ^ input ^ ": " ^ e2))
     ) (Ok ()) inputs
 
-let read_t = Cmdliner.Term.(const main $ input)
+let read_t = Cmdliner.Term.(const main $ verbose $ input)
 
 let () =
   Cmdliner.Term.eval (read_t, info) |> function
