8616a96cbb605f0bf136efef322b13bf562c26ec
yomimono <maintenance@identity-function.com>
2021-09-27 14:51:14 -0500

    overhaul 'listing' to be more legible
    Rename the extra bits we add to fulfill the aspect ratio to 'matting',
    since we already call something else 'background'.
    Make the matting contrast with the text color we'll use for the annotation.
    Remove special handling for images that are already the right aspect
    ratio.

--- listing/annotate.ml
+++ listing/annotate.ml
@@ -1,64 +1,90 @@
-let pdf_glyph_dims = (5, 7)
+let glyph_width, glyph_height = (5, 7)
 
-let add_pdf image ~orientation (r, g, b) =
+let p_glyph = [
+  (0, 0); (1, 0); (2, 0);
+  (0, 1);         (2, 1);
+  (0, 2); (1, 2); (2, 2);
+  (0, 3);
+  (0, 4);
+]
+and d_glyph = [
+  (0, 0); (1, 0);
+  (0, 1);         (2, 1);
+  (0, 2);         (2, 2);
+  (0, 3);         (2, 3);
+  (0, 4); (1, 4);
+]
+and f_glyph = [
+  (0, 0); (1, 0); (2, 0);
+  (0, 1);
+  (0, 2); (1, 2);
+  (0, 3);
+  (0, 4);
+]
+and bang = [
+  (1, 0);
+  (1, 1);
+  (1, 2);
+
+  (1, 4);
+]
+
+let glyphs = [p_glyph; d_glyph; f_glyph; bang]
+
+(*  since "background" already means something else,
+ *  reuse a term from framing for the extra stuff we're sticking
+ *  in to make the thing fit a particular aspect ratio. *)
+let color_matting image orientation color =
+  let paint_rectangle image (r, g, b) ~start_x ~start_y ~last_x ~last_y =
+    for row = start_y to last_y do
+      for column = start_x to last_x do
+        Image.write_rgb image column row r g b
+      done
+    done
+  in
+  let fill = paint_rectangle image color in
+  let image_max_x, image_max_y = (image.Image.width - 1, image.Image.height - 1) in
+  match orientation with
+  | `Vertical (left_edge, right_edge) ->
+    (* write pixels from (0, 0) through (left_edge, image.max) and (right_edge, 0) through (image.max, image.max) *)
+    fill ~start_x:0 ~start_y:0 ~last_x:left_edge ~last_y:image_max_y;
+    fill ~start_x:right_edge ~start_y:0 ~last_x:image_max_x ~last_y:image_max_y
+  | `Horizontal (top_edge, bottom_edge) ->
+    (* write pixels from (0, 0) through (image.max, top_edge) and (bottom_edge, 0) through (image.max, image.max) *)
+    fill ~start_x:0 ~start_y:0 ~last_x:image_max_x ~last_y:top_edge;
+    fill ~start_x:0 ~start_y:bottom_edge ~last_x:image_max_x ~last_y:image_max_y
+
+let write image glyphs orientation (r, g, b) ~start_x ~start_y =
+  let translate_to_image_coordinates nth (x, y) = match orientation with
+    | `Horizontal _ -> ((start_x + x + nth * glyph_width), (y + start_y))
+    | `Vertical _ -> ((start_x + x), (nth * glyph_height) + (start_y + y))
+  in
+  List.iteri (fun nth glyph ->
+      List.iter (fun coords ->
+          let (x, y) = translate_to_image_coordinates nth coords in
+          Image.write_rgb image x y r g b)
+        glyph
+    ) glyphs
+
+let add_pdf image ~orientation ~text_color ~matting_color =
   (* need 5 vertical pixels and 3 horizontal for each glyph *)
   (* this is *ludicrously* bad internationalization,
      but I'm assuming that .pdf is a global idea...? *)
-  let p_glyph = [
-    (0, 0); (1, 0); (2, 0);
-    (0, 1);         (2, 1);
-    (0, 2); (1, 2); (2, 2);
-    (0, 3);
-    (0, 4);
-  ]
-  and d_glyph = [
-    (0, 0); (1, 0);
-    (0, 1);         (2, 1);
-    (0, 2);         (2, 2);
-    (0, 3);         (2, 3);
-    (0, 4); (1, 4);
-  ]
-  and f_glyph = [
-    (0, 0); (1, 0); (2, 0);
-    (0, 1);
-    (0, 2); (1, 2);
-    (0, 3);
-    (0, 4);
-  ]
-  and bang = [
-    (1, 0);
-    (1, 1);
-    (1, 2);
-
-    (1, 4);
-  ]
-  and glyph_width, glyph_height = pdf_glyph_dims
-  in
-  let glyphs = [p_glyph; d_glyph; f_glyph; bang] in
-  let write ~start_x ~start_y ~orientation glyphs =
-    let translate_to_image_coordinates nth (x, y) = match orientation with
-      | `Horizontal _ -> ((start_x + x + nth * glyph_width), (y + start_y))
-      | `Vertical _ -> ((start_x + x), (nth * glyph_height) + (start_y + y))
-    in
-    List.iteri (fun nth glyph ->
-        List.iter (fun coords ->
-            let (x, y) = translate_to_image_coordinates nth coords in
-            Image.write_rgb image x y r g b)
-          glyph
-      ) glyphs
-  in
-  match orientation with
-  | `Horizontal top_edge ->
+  let start_x, start_y = match orientation with
+  | `Horizontal (top_edge, _) ->
     (* center the text horizontally *)
-    let start_x = (image.width / 2) - (((List.length glyphs) / 2) * glyph_width)
+    let start_x = (image.Image.width / 2) - (((List.length glyphs) / 2) * glyph_width)
     (* vertically, make sure it's above the original image *)
     and start_y = max 0 (top_edge / 2 - glyph_height)
     in
-    write ~start_x ~start_y ~orientation glyphs
-  | `Vertical left_edge ->
+    (start_x, start_y)
+  | `Vertical (left_edge, _) ->
     (* make sure the text is to the left of the original image *)
     let start_x = max 0 (left_edge / 2 - glyph_width)
     (* center the text vertically *)
-    and start_y = (image.height / 2) - (((List.length glyphs) / 2) * glyph_height)
+    and start_y = (image.Image.height / 2) - (((List.length glyphs) / 2) * glyph_height)
     in
-    write ~start_x ~start_y ~orientation glyphs
+    (start_x, start_y)
+  in
+  color_matting image orientation matting_color;
+  write image glyphs orientation text_color ~start_x ~start_y
--- listing/listing.ml
+++ listing/listing.ml
@@ -14,28 +14,43 @@ let output =
    opaque as well. *)
 let alpha = 255
 
+(* TODO: reuse the contrast math we have for the pdf generator *)
+let contrast = function
+  | x, y, z when x <= 128 && y <= 128 && z <= 128 -> 255,255,255
+  | _ -> 0, 0, 0 
+
 (* extra_width or extra_height are amounts of additional border to create *)
 let make_background ?(extra_width=0) ?(extra_height=0) substrate =
   let width = substrate.max_x + 1 + extra_width
   and height = substrate.max_y + 1 + extra_height
-  and r, g, b = substrate.background in
+  and r, g, b = substrate.background
+  in
   let image = Image.create_rgb ~alpha:false width height in
   Image.fill_rgb image r g b;
   image
 
+(* TODO: etsy accepts several aspect ratios as "optimal", not just 4:3;
+ * we could probably do something more intelligent here *)
 let solve_for_four_to_three substrate =
   let open Annotate in
   let width = substrate.max_x + 1 and height = substrate.max_y + 1 in
   let ratio = (float_of_int width) /. (float_of_int height) in
   let better_height = int_of_float @@ 0.75 *. (float_of_int width)
-  and better_width = int_of_float @@ 1.25 *. (float_of_int height )
+  and better_width = int_of_float @@ 1.25 *. (float_of_int height)
+  and min_height = (Annotate.glyph_height + 2) * 2 (* times 2 because it needs to be 
+                                             of equal size on either side of the
+                                             original image, but also needs
+                                             to be large enough to contain the
+                                             annotation *)
+  and min_width = (Annotate.glyph_width + 2) * 2
   in
-  if ratio <= 1.1 && (better_width - (fst pdf_glyph_dims)) > width then
-    `Widen (better_width - width)
-  else if ratio >= 1.5 && (better_height - (snd pdf_glyph_dims)) > height then
-    `Heighten (better_height - height)
-  else
-    `Just_right ((better_width - width), (better_height - height))
+  if ratio <= 1.1 && (better_width - glyph_width) > width then
+    `Widen (max (better_width - width) min_width)
+  else if ratio >= 1.5 && (better_height - glyph_height) > height then
+    `Heighten (max (better_height - height) min_height)
+  (* for cases that are already just about right, we still need to make sure
+   * we have enough room to annotate them *)
+  else `Heighten (max (better_height - height) min_height)
 
 let paint_pixel image adjustment thread (x, y) =
   let (r, g, b) = Stitchy.DMC.Thread.to_rgb thread in
@@ -44,8 +59,6 @@ let paint_pixel image adjustment thread (x, y) =
     Image.write_rgba image (x + (extra_width / 2)) y r g b alpha
   | `Heighten extra_height ->
     Image.write_rgba image x (y + (extra_height / 2)) r g b alpha
-  | _ ->
-    Image.write_rgba image x y r g b alpha
 
 let paint_line _image _adjustment _thread (src_x, src_y) =
   let _ = (src_x, src_y) in
@@ -69,26 +82,19 @@ let go input output =
   let image = match adjustment with
     | `Widen extra_width -> make_background ~extra_width substrate
     | `Heighten extra_height -> make_background ~extra_height substrate
-    | `Just_right _ ->
-      let extra_width = (fst Annotate.pdf_glyph_dims) * 2
-      and extra_height = (snd Annotate.pdf_glyph_dims) * 2
-      in
-      make_background ~extra_width ~extra_height substrate
   in
-  let text_color =
-    (* TODO: reuse the contrast math we have for the pdf generator *)
-    match substrate.background with
-    | 0,0,0 -> 255,255,255
-    | 255,255,255 -> 0,0,0
-    | color -> color
-  in
+  let text_color = substrate.background in
   let orientation = match adjustment with
-    | `Widen margin -> `Vertical (margin / 2)
-    | `Heighten margin -> `Horizontal (margin / 2)
-    | `Just_right _ -> (* arbitrarily, horizontal *)
-      `Horizontal (snd Annotate.pdf_glyph_dims)
+    | `Widen margin ->
+      let left_edge = margin / 2 in
+      let right_edge = left_edge + substrate.max_x + 1 in
+      `Vertical (left_edge, right_edge)
+    | `Heighten margin ->
+      let top_edge = margin / 2 in
+      let bottom_edge = top_edge + substrate.max_y + 1 in
+      `Horizontal (top_edge, bottom_edge)
   in
-  Annotate.add_pdf image ~orientation text_color;
+  Annotate.add_pdf image ~orientation ~text_color ~matting_color:(contrast text_color);
   List.iter (paint_layer image adjustment) layers;
   List.iter (paint_backstitch_layer image adjustment) backstitch_layers;
   let () = ImageLib_unix.writefile output image in
