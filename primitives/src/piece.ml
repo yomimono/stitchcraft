724d9c1b8730447cdb6e92e18b1548d636714a9c
yomimono <maintenance@identity-function.com>
2021-12-08 18:51:51 -0600

    submap doesn't displace; piece needs to do that

--- primitives/src/piece.ml
+++ primitives/src/piece.ml
@@ -30,11 +30,13 @@ let piece x_off y_off width height file =
   | Error e -> failwith @@ Printf.sprintf "couldn't parse input file: %s" e
       (* TODO: fuck backstitch *)
   | Ok { substrate; layers; _ } ->
+    let displace (m, n) = (m - x_off), (n - y_off) in
     let l = Stitchy.Types.submap ~x_off ~y_off ~width ~height layers in
     let substrate = Stitchy.Types.({substrate with max_x = max 0 @@ width - 1;
                                     max_y = max 0 @@ height - 1;
                     }) in
-    let p = Stitchy.Types.({ substrate; layers = l; backstitch_layers = [] }) in
+    let p = Stitchy.Types.({ substrate; layers = l; backstitch_layers = [] }) |>
+      (Stitchy.Operations.transform_all_stitches ~f:displace) in
     Yojson.Safe.to_channel stdout @@ Stitchy.Types.pattern_to_yojson p
 
 let info = Cmdliner.Term.info "piece" ~doc:"slice a piece out of an existing pattern"
