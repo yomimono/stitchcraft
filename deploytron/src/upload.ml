df9a9f85309aaa1877da286ce78fb3239a3fb48b
yomimono <maintenance@identity-function.com>
2022-02-11 18:52:13 -0600

    an error occurred, but it's less obviously my fault

--- deploytron/src/upload.ml
+++ deploytron/src/upload.ml
@@ -62,39 +62,35 @@ let upload storage key shop listing filename =
       match Bos.OS.File.read (Fpath.v filename) with
       | Error (`Msg s) -> Printf.eprintf "couldn't read file for upload: %s\n%!" s; exit 1
       | Ok file ->
-        Printf.printf "file size: %d\n%!" @@ String.length file;
         let headers = auth_headers key token in
         let uri = Etsy.File.upload_by_listing_id ~shop listing in
         (* in order to upload properly, we need multipart form data,
          * which cohttp doesn't directly provide.
          * dinosaure to the rescue ;) *)
         (* the (string * int * int ) in the part definition is buffer, offset, length *)
+        let filename = Fpath.basename @@ Fpath.v filename in
         let name_part, rank_part, file_part =
           let rank_str = string_of_int rank in
           let open Multipart_form in
-          part ~encoding:`Bit7 @@ stream_of_string filename,
-          part ~encoding:`Bit7 @@ stream_of_string rank_str,
+          part ~encoding:`Bit7 ~disposition:(Content_disposition.v "name") @@ stream_of_string filename,
+          part ~encoding:`Bit7 ~disposition:(Content_disposition.v "rank") @@ stream_of_string rank_str,
           part ~encoding:`Binary ~disposition:(Content_disposition.v ~filename "file") @@
             stream_of_string file
         in
         Mirage_crypto_rng_unix.initialize ();
         let rng ?g:_ n = Mirage_crypto_rng_unix.getrandom n |> Cstruct.to_string |> Base64.encode_string ~alphabet:Base64.uri_safe_alphabet in
-        Printf.printf "randomed\n%!";
         let multipart = Multipart_form.multipart ~rng [name_part; rank_part; file_part] in
-        Printf.printf "multiparted\n%!";
         let h, form_body = Multipart_form.to_stream multipart in
-        let content_type = Multipart_form.(Header.content_type h |> Content_type.to_string)
-        and content_encoding = Multipart_form.(Header.content_encoding h |> Content_encoding.to_string)
-        in
-        let headers = Cohttp.Header.add_list headers
-            ["Content-type", content_type;
-             "Content-encoding", content_encoding;
-            ] in
-        let body = Lwt_stream.(from_direct form_body |> map (fun (buf, _, _) -> buf)) |> Cohttp_lwt.Body.of_stream in
+        let content_type = Multipart_form.(Header.content_type h |> Content_type.to_string) in
+        let headers = Cohttp.Header.add headers "Content-Type" content_type in
+        Format.printf "%a\n%!" Cohttp.Header.pp_hum headers;
+        let body = Lwt_stream.(from_direct form_body |> map (fun (buf, _, _) ->
+            Printf.printf "%s%!" buf;
+            buf)) |> Cohttp_lwt.Body.of_stream in
         Cohttp_lwt_unix.Client.post ~headers ~body uri >>= fun (response, body) ->
         Cohttp_lwt.Body.to_string body >>= fun contents ->
         success_or_death response contents >>= fun () ->
-        Printf.printf "%s\n%!" contents;
+        Printf.printf "\n\n%s\n%!" contents;
         Lwt.return_unit
     )
 
