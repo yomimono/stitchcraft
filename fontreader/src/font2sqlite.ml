cf5166f448e5f78ffe8d9dd50163754d287e7dc4
yomimono <maintenance@identity-function.com>
2022-02-16 17:33:26 -0700

    have only one binary for ingesting fonts

--- fontreader/src/font2sqlite.ml
+++ fontreader/src/font2sqlite.ml
@@ -1,3 +1,7 @@
+let fontformat = Cmdliner.Arg.enum ["otf", `Otf;
+                                    "psf", `Psf;
+                                   ]
+
 let debug =
   let doc = "print debug output on stdout" in
   Cmdliner.Arg.(value & flag & info ["debug"; "d"] ~doc ~docv:"VERBOSE")
@@ -7,21 +11,33 @@ let db =
   Cmdliner.Arg.(value & opt string "fonts.sqlite3" & info ["db"] ~doc ~docv:"DB")
 
 let src =
-  let doc = "source PSF version 2 file (with unicode table)" in
+  let doc = "source font" in
   Cmdliner.Arg.(value & pos 0 file "fonts" & info [] ~doc ~docv:"SRC")
+
+let fmt =
+  let doc = "type of font parser to use" in
+  Cmdliner.Arg.(value & opt fontformat `Otf & info ["format"] ~doc ~docv:"FORMAT")
 
 let font_name =
   let doc = "name by which to refer to this font" in
   Cmdliner.Arg.(value & opt string "c64" & info ["n"; "name"] ~doc ~docv:"FONT_NAME")
 
-module Populator = Sqlite.Populate(Fontreader.Psf2stitchfont)
+let ingest fmt db src font_name debug =
+  let open Fontreader in
+  match fmt with
+  | `Otf ->
+    let module Populator = Sqlite.Populate(Otf2stitchfont) in
+    Populator.populate db src font_name debug
+  | `Psf ->
+    let module Populator = Sqlite.Populate(Psf2stitchfont) in
+    Populator.populate db src font_name debug
 
-let populate_t = Cmdliner.Term.(const Populator.populate $ db $ src $ font_name $ debug)
+let ingest_t = Cmdliner.Term.(const ingest $ fmt $ db $ src $ font_name $ debug)
 
 let info = Cmdliner.Term.info "populate a sqlite database with font information"
 
 let () =
-  Cmdliner.Term.eval (populate_t, info) |> function
+  Cmdliner.Term.eval (ingest_t, info) |> function
   | `Ok (Error (`Msg s)) -> 
     Format.eprintf "%s\n%!" s; exit 1
   | `Ok (Ok ()) ->
