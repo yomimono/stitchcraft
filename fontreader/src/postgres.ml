8e69f98c23389452b7cf462a76b0d93da24f19cc
yomimono <maintenance@identity-function.com>
2022-03-13 03:38:46 -0500

    use a cte to do all the stuff at once

--- fontreader/src/postgres.ml
+++ fontreader/src/postgres.ml
@@ -15,7 +15,7 @@ let make_glyphs_table connection =
         height          integer NOT NULL,
         width           integer NOT NULL,
         stitches        json NOT NULL,
-        CONSTRAINT positive CHECK (height > 0 AND width > 0)
+        CONSTRAINT positive CHECK (height >= 0 AND width >= 0)
         )
     |}
 
@@ -38,47 +38,28 @@ let make_fonts_glyphs_table connection =
         )
     |}
 
-let new_font connection name =
-  let param = Pgx.Value.of_string name in
-  Pgx_lwt_unix.execute connection ~params:[param]
-    "INSERT INTO FONTS (name) VALUES ($1) ON CONFLICT (name) DO NOTHING RETURNING id"
-  >>= function
-  | [] ->
-    (* TODO it'd be nice to have an overwrite/update option *)
-    Lwt.return @@ Error (`Msg (Format.asprintf "a font named %s already exists" name))
-  | _::_::_ -> Lwt.return @@ Error (`Msg "made more than one font")
-  | row::[] ->
-    match row with
-    | id::[] -> Lwt.return @@ Option.to_result ~none:(`Msg "font id wasn't an int somehow")
-      @@ Pgx.Value.to_int id
-    | _ -> Lwt.return @@ Error (`Msg "new font creation didn't return an id")
-
-let associate_glyph ~glyph_id ~font_id connection uchars =
-  let query = "INSERT INTO fonts_glyphs (font, uchar, glyph) VALUES ($1, $2, $3)" in
-  let params uchar =
-    [Pgx.Value.of_int font_id;
-     Pgx.Value.of_int (Uchar.to_int uchar);
-     Pgx.Value.of_int glyph_id] in
-  Pgx_lwt_unix.execute_many connection ~params:(List.map params uchars) ~query >>= function
-  | l when not @@ Int.equal (List.length l) (List.length uchars) ->
-    Lwt.return @@ Error (`Msg (Format.asprintf "fewer association rows than expected were created for glyph %d" glyph_id))
-  | _ -> Lwt.return @@ Ok ()
-
-let new_glyph connection (Stitchy.Types.{width; height; stitches}) =
-  let stitches_json = Stitchy.Types.CoordinateSet.to_yojson stitches in
-  let stitches_string = Yojson.Safe.to_string stitches_json in
-  let params = [Pgx.Value.of_int width; Pgx.Value.of_int height;
-                Pgx.Value.of_string stitches_string] in
-  let query =
-    "INSERT INTO glyphs (width, height, stitches) VALUES ($1, $2, $3) RETURNING id"
+let insert connection font Stitchy.Types.({width; height; stitches}) uchars =
+  let glyph_cte = {|
+  WITH glyph AS (
+    INSERT INTO glyphs (width, height, stitches) VALUES ($1, $2, $3) RETURNING id
+  ), font AS (
+    INSERT INTO FONTS (name) VALUES ($4) ON CONFLICT (name) DO NOTHING RETURNING id
+  )
+  INSERT INTO fonts_glyphs (font, glyph, uchar)
+    SELECT font.id, glyph.id, $5
+    FROM font, glyph
+  |}
   in
-  Pgx_lwt_unix.execute connection ~params query >>= function
-  | [] | _::_::_ -> Lwt.return @@ Error (`Msg "failed to insert a glyph")
-  | row::[] ->
-    match row with
-    | id::[] -> Lwt.return @@ Option.to_result ~none:(`Msg "glyph id wasn't an int somehow")
-      @@ Pgx.Value.to_int id
-    | _ -> Lwt.return @@ Error (`Msg "new glyph creation didn't return an id")
+  let stitches_json = Stitchy.Types.CoordinateSet.to_yojson stitches |> Yojson.Safe.to_string in
+  let params uchar = Pgx.Value.[of_int width;
+                          of_int height;
+                          of_string stitches_json;
+                          of_string font;
+                          of_int (Uchar.to_int uchar);]
+  in
+  Pgx_lwt_unix.execute_many connection ~params:(List.map params uchars) ~query:glyph_cte >|= function
+  | l when (List.length l) < (List.length uchars) -> Error (`Msg "not as many rows were inserted as we expected")
+  | _ -> Ok ()
 
 module Populate(Reader : Fontreader.Readfiles.INTERPRETER) = struct
   let populate {host; port; user; database; password} src font debug =
@@ -93,22 +74,13 @@ module Populate(Reader : Fontreader.Readfiles.INTERPRETER) = struct
           make_glyphs_table connection >>= fun () ->
           make_fonts_table connection >>= fun () ->
           make_fonts_glyphs_table connection >>= fun () ->
-          new_font connection font >>= function
-          | Error _ as e -> Lwt.return e
-          | Ok font_id ->
-            if debug then Format.printf "created new font at font_id %d\n%!" font_id;
-            Lwt_list.iter_p (fun (glyph, uchars) ->
-              new_glyph connection glyph >>= function
+          Lwt_list.iter_p (fun (glyph, uchars) ->
+              insert connection font glyph uchars>>= function
               | Error (`Msg s) ->
                 Format.eprintf "Error %s: couldn't insert glyph for %a; skipping those uchars\n%!" s Fmt.(list int) (List.map Uchar.to_int uchars);
                 Lwt.return_unit
-              | Ok glyph_id -> begin
-                associate_glyph ~glyph_id ~font_id connection uchars >>= function
-                | Error (`Msg s) -> Format.eprintf "Error %s: couldn't associate glyphs for %a" s Fmt.(list int) (List.map Uchar.to_int uchars);
-                  Lwt.return_unit
-                | Ok _ -> Lwt.return_unit
-              end
+              | Ok () -> Lwt.return_unit
             ) l >>= fun _ ->
-            Lwt.return @@ Ok ()
+          Lwt.return @@ Ok ()
         )
 end
