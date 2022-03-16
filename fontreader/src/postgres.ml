8fac21ea5194b1a36f7f3cd15cfca3baebde8776
yomimono <maintenance@identity-function.com>
2022-03-15 19:37:58 -0500

    a better-working insert query

--- fontreader/src/postgres.ml
+++ fontreader/src/postgres.ml
@@ -34,32 +34,55 @@ let make_fonts_glyphs_table connection =
         font integer references fonts(id),
         uchar integer NOT NULL,
         glyph integer references glyphs(id),
-        CONSTRAINT positive CHECK (uchar > 0)
+        CONSTRAINT positive CHECK (uchar >= 0)
         )
     |}
 
-let insert connection font Stitchy.Types.({width; height; stitches}) uchars =
+let insert debug connection font glyphmap =
   let glyph_cte = {|
-  WITH glyph AS (
+  WITH selected_font AS (
+    SELECT id FROM fonts WHERE name = $4 LIMIT 1
+  ),
+  inserted_font AS (
+    INSERT INTO fonts (name) VALUES ($4)
+    ON CONFLICT DO NOTHING
+    RETURNING id
+  ),
+  glyph AS (
     INSERT INTO glyphs (width, height, stitches) VALUES ($1, $2, $3) RETURNING id
-  ), font AS (
-    INSERT INTO FONTS (name) VALUES ($4) ON CONFLICT (name) DO NOTHING RETURNING id
   )
   INSERT INTO fonts_glyphs (font, glyph, uchar)
-    SELECT font.id, glyph.id, $5
-    FROM font, glyph
+    SELECT selected_font.id, glyph.id, $5::integer FROM selected_font, glyph
+    UNION
+    SELECT inserted_font.id, glyph.id, $5::integer FROM inserted_font, glyph
+    LIMIT 1
   |}
   in
-  let stitches_json = Stitchy.Types.CoordinateSet.to_yojson stitches |> Yojson.Safe.to_string in
-  let params uchar = Pgx.Value.[of_int width;
-                          of_int height;
-                          of_string stitches_json;
-                          of_string font;
-                          of_int (Uchar.to_int uchar);]
+  (* if we get a (glyph * uchar list) list,
+   * we need to generate the list of glyph-uchar pairings for each element,
+   * then generate those for each glyph,
+   * flatten the list,
+   * and use that as params *)
+  let params =
+    let params_per_uchar Stitchy.Types.({width; height; stitches}) uchar =
+      let stitches_json = Stitchy.Types.CoordinateSet.to_yojson stitches |> Yojson.Safe.to_string in
+      Pgx.Value.[of_int width;
+                 of_int height;
+                 of_string stitches_json;
+                 of_string font;
+                 of_int (Uchar.to_int uchar);]
+    in
+    let params_per_glyph (glyph, uchars) = List.map (params_per_uchar glyph) uchars in
+    if debug then Format.eprintf "inserting %d rows for %d glyphs\n%!"
+        List.(length @@ flatten @@ map params_per_glyph glyphmap)
+        (List.length glyphmap);
+    List.flatten @@ List.map params_per_glyph glyphmap
   in
-  Pgx_lwt_unix.execute_many connection ~params:(List.map params uchars) ~query:glyph_cte >|= function
-  | l when (List.length l) < (List.length uchars) -> Error (`Msg "not as many rows were inserted as we expected")
-  | _ -> Ok ()
+  if debug then Format.eprintf "about to insert %d rows\n%!" List.(length params);
+  Pgx_lwt_unix.execute_many connection ~params ~query:glyph_cte >|= function
+  | l ->
+    if debug then Format.eprintf "%d rows returned\n%!" List.(length @@ flatten l);
+    Ok ()
 
 module Populate(Reader : Fontreader.Readfiles.INTERPRETER) = struct
   let populate {host; port; user; database; password} src font debug =
@@ -70,17 +93,10 @@ module Populate(Reader : Fontreader.Readfiles.INTERPRETER) = struct
       | Error e -> Error (`Msg (Format.asprintf "%a" Reader.pp_error e))
       | Ok (l : (Stitchy.Types.glyph * Uchar.t list) list) ->
         Lwt_main.run (
-          Pgx_lwt_unix.connect ~host ~port ~user ~database ~password () >>= fun connection ->
+          Pgx_lwt_unix.connect ~verbose:10 ~host ~port ~user ~database ~password () >>= fun connection ->
           make_glyphs_table connection >>= fun () ->
           make_fonts_table connection >>= fun () ->
           make_fonts_glyphs_table connection >>= fun () ->
-          Lwt_list.iter_p (fun (glyph, uchars) ->
-              insert connection font glyph uchars>>= function
-              | Error (`Msg s) ->
-                Format.eprintf "Error %s: couldn't insert glyph for %a; skipping those uchars\n%!" s Fmt.(list int) (List.map Uchar.to_int uchars);
-                Lwt.return_unit
-              | Ok () -> Lwt.return_unit
-            ) l >>= fun _ ->
-          Lwt.return @@ Ok ()
+          insert debug connection font l
         )
 end
