4048584b99d8b5e7fbeb3196aed56fdc86278a17
yomimono <maintenance@identity-function.com>
2022-03-29 15:43:28 -0500

    draw backstitch in display

--- webapp/display/canvas.ml
+++ webapp/display/canvas.ml
@@ -6,6 +6,10 @@ type t = {
   canvas : Js_of_ocaml.Dom_html.canvasElement Js_of_ocaml.Js.t;
 }
 
+let no_index_width = 1.
+let minor_index_width = 2.
+let major_index_width = 3.
+
 let thread_to_css thread = Js_of_ocaml.CSS.Color.hex_of_rgb (DMC.Thread.to_rgb thread)
 
 let render_stitch {canvas; block_size} _stitch thread (x, y) =
@@ -17,9 +21,20 @@ let render_stitch {canvas; block_size} _stitch thread (x, y) =
     (* width *) (float_of_int block_size)
     (* length *) (float_of_int block_size)
 
+let render_backstitch {canvas; block_size} thread ((x1, y1), (x2, y2)) =
+  let context = canvas##getContext (Js_of_ocaml.Dom_html._2d_) in
+  context##.strokeStyle := Js_of_ocaml.Js.string @@ thread_to_css thread;
+  context##.lineWidth := no_index_width;
+  context##beginPath;
+  context##moveTo (float_of_int (x1 * block_size)) (float_of_int (y1 * block_size));
+  context##lineTo (float_of_int (x2 * block_size)) (float_of_int (y2 * block_size));
+  context##stroke;
+  context##closePath;
+  ()
+
 let render_grid ?(minor_index= 5) ?(major_index = 20) {canvas; block_size} piece =
   let linewidth index = match index mod minor_index with
-    | 0 -> if index mod major_index = 0 then 3. else 2.
+    | 0 -> if index mod major_index = 0 then major_index_width else minor_index_width;
     | _ -> 1.
   in
   let context = canvas##getContext (Js_of_ocaml.Dom_html._2d_) in
@@ -56,6 +71,9 @@ let render_grid ?(minor_index= 5) ?(major_index = 20) {canvas; block_size} piece
 let render_layer canvas layer =
   CoordinateSet.iter (render_stitch canvas layer.stitch layer.thread) layer.stitches
 
+let render_backstitch_layer canvas (backstitch_layer : backstitch_layer) =
+  SegmentSet.iter (render_backstitch canvas backstitch_layer.thread) backstitch_layer.stitches
+
 let render_background {canvas; block_size} substrate =
   (* draw a big ol' rectangle of the background color *)
   let far_x = (substrate.max_x + 1) * block_size
@@ -72,4 +90,6 @@ let render_background {canvas; block_size} substrate =
 let render canvas state =
   render_background canvas state.substrate;
   render_grid canvas state.substrate;
-  List.iter (render_layer canvas) state.layers
+  List.iter (render_layer canvas) state.layers;
+  List.iter (render_backstitch_layer canvas) state.backstitch_layers;
+  ()
