160e48c58a9c70f6f6c88feabb13ec3912f90e63
yomimono <maintenance@identity-function.com>
2022-10-13 15:16:31 -0500

    bring assembler into generators

---
+++ src/assembler.ml
@@ -0,0 +1,9 @@
+let go input grid width height background exclude output =
+  match Stitchy.Files.stdin_or_file input with
+  | Error s -> failwith s
+  | Ok json ->
+    match Stitchy.Types.layers_of_yojson json with
+    | Error s -> failwith s
+    | Ok layers ->
+      let pattern = Assemble.stitch background width height grid exclude layers in
+      Stitchy.Files.stdout_or_file (Stitchy.Types.pattern_to_yojson pattern) output
--- src/stitchcraft.ml
+++ src/stitchcraft.ml
@@ -2,11 +2,12 @@
 ### generation
 - `textstitch` - make a pattern from a list of strings
 - `assemble` - take a list of layers (e.g. from `ih`) and make a pattern
-- `primitives`: `rect`, `piece`, `rotate`, `backstitch`, `empty` - simple figures or empty space
+- `primitives`: `rect`, `backstitch`, `empty` - simple figures or empty space
 
 ### manipulation
 
 - `embellish` (hcat, vcat, repeat_corner, embellish_stitch`)
+- `piece`, `rotate`
 
 ### output
 
@@ -27,6 +28,12 @@
    *)
 open Cmdliner
 
+let input =
+  Cmdliner.Arg.(value & opt string "-" & info ["i"; "input"] ~doc:"Input file. Default stdin" ~docv:"INPUT")
+
+let output =
+  Cmdliner.Arg.(value & opt string "-" & info ["o"; "output"] ~doc:"Output file. Default stdout" ~docv:"OUTPUT")
+
 module Generation = struct
 
   let segment =
@@ -37,10 +44,11 @@ module Generation = struct
     let doc = "list of backstitches as pairs of sources and destinations on the line grid, e.g. 0,0/1,1" in
     Cmdliner.Arg.(value & pos_all segment [] & info [] ~doc ~docv:"SEGMENTS")
 
+  let thread_conv = Cmdliner.Arg.conv Stitchy.DMC.Thread.(parse, pp)
+
   let thread =
-    let thread_conv = Cmdliner.Arg.conv Stitchy.DMC.Thread.(parse, pp) in
     let default = List.hd Stitchy.DMC.Thread.basic in
-    let doc = "thread color for the stitches" in
+    let doc = "thread color" in
     Cmdliner.Arg.(value & opt thread_conv default & info ["t"; "thread"] ~doc ~docv:"THREAD")
 
   let background =
@@ -58,14 +66,24 @@ module Generation = struct
     Cmdliner.Arg.(value & opt (enum grid_converter) Stitchy.Types.Fourteen & info ["g"; "gridsize"] ~doc)
 
   let width =
-    let doc = "width of the empty grid, in number of (non-) cross-stitches" in
+    let doc = "width of the pattern, in number of (non-) cross-stitches" in
     Cmdliner.Arg.(value & pos 0 int 1 & info [] ~doc ~docv:"WIDTH")
 
   let height =
-    let doc = "height of the empty grid, in number of (non-) cross-stitches" in
+    let doc = "height of the pattern, in number of (non-) cross-stitches" in
     Cmdliner.Arg.(value & pos 1 int 1 & info [] ~doc ~docv:"HEIGHT")
 
+  let exclude =
+    let open Cmdliner.Arg in
+    let doc = "thread identifiers to omit from the pattern. This is useful when consuming output from a program with no support for transparency - put stitch colors you expect to be the background here." in
+    Cmdliner.Arg.(value & opt_all thread_conv [] & info ["e"; "exclude"] ~doc)
+
 end
+
+let assemble_program = Assembler.go
+let assemble_info = Cmdliner.Cmd.info "assemble"
+let assemble_cmd =
+  Cmdliner.Cmd.v assemble_info @@ Term.(const assemble_program $ input $ grid $ width $ height $ background $ exclude $ output)
 
 let backstitch_program = Backstitch.bs
 let empty_program width height bg gridsize =
@@ -82,7 +100,7 @@ let empty_cmd =
   let open Generation in
   Cmdliner.Cmd.v empty_info @@ Term.(const empty_program $ width $ height $ background $ gridsize)
 
-let generators = Cmdliner.Cmd.(group (info "stitchcraft") [backstitch_cmd; empty_cmd])
+let generators = Cmdliner.Cmd.(group (info "stitchcraft") [assemble_cmd; backstitch_cmd; empty_cmd])
 
 let () =
   exit @@ Cmdliner.Cmd.eval generators
